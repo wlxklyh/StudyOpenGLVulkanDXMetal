a2b9e23e519aa14789382910a62bb1b3d83f592c
wlxklyh <linyanhou@qq.com>
2024-07-24 02:36:28 +0800

    Vulkan template

--- Vulkan/VulkanDemo/Template/Main.cpp
+++ Vulkan/VulkanDemo/Template/Main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	std::cout << "Hello World!";
+#include "GlfwGeneral.hpp"
+
+int main() {
+	if (!InitializeWindow({1280,720}))
+		return -1;//来个你讨厌的返回值
+	while (!glfwWindowShouldClose(pWindow)) {
+
+		/*渲染过程，待填充*/
+
+		glfwPollEvents();
+		TitleFps();
+	}
+	TerminateWindow();
 	return 0;
 }
